f9b08290ab0e8b8f6ec5593320befb47b5630541
CHINTAPALLI KAVYA DURGA SRI LAKSHMI <24b05a1204@svecw.edu.in>
2026-08-31 03:37:59 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 
-const API_URL = "http://localhost:8080/api";
+const API_URL = " https://codesentinel-ai-4.onrender.com";
 /* =========================================================
    DEFAULT STARTER CODE FOR EACH LANGUAGE
    ========================================================= */

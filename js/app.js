b2004ec07089577f53dd743bf529421f677015b8
CHINTAPALLI KAVYA DURGA SRI LAKSHMI <24b05a1204@svecw.edu.in>
2026-08-31 03:31:42 +0530

    Fix API_URL declaration formatting

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
-const API_URL = "http://localhost:8080/api";
 
+const API_URL = "http://localhost:8080/api";
 /* =========================================================
    DEFAULT STARTER CODE FOR EACH LANGUAGE
    ========================================================= */
